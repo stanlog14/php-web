e2f461aa430146d5a303bac3a8340868011e7c0b
Stanislas Logghe <slogghe@sio.local>
2021-10-28 09:24:29 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr"
+<head>
+	<title> TDS PHP </title>
+	<metacharset="utf-8">
+</head>
+<body>
+	<h1> Essais de la méthode GET </h1>
+	<ul>
+  	<li>
+      	<a href="test.php?msg=Hello World!"> Get en php </a>
+  	</li>
+	</ul>
+</body>
+
+
+</html>
---
+++ test.php
@@ -0,0 +1,6 @@
+<h1>Message affiché</h1>
+<?php
+if(isset($_GET['msg'])){
+echo $_GET['msg'];
+}
+?>
